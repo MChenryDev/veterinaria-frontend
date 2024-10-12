a2e76ffb2459ad25f3db49b4a8fad4f1a19bcd57
MChenryDev <henry4621.com@gmail.com>
2024-10-11 18:41:32 -0600

    Ultimos cambios realizados 11102024

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import CrearCita from './components/Cita/CrearCita';
 import ListarCitas from './components/Cita/ListarCitas';
 import Facturacion from './components/Facturacion/Facturacion';
 import Facturas from './components/Facturacion/Facturas';
-import Productos from './components/Facturacion/Productos';
+import Servicios from './components/Facturacion/Servicios';
 import FacturaEdit from './components/Facturacion/FacturaEdit';
 import Dashboard from './components/Dashboard/Dashboard';
 /*
@@ -31,8 +31,26 @@ function App() {
     <Router>
       <div>
         <h1>Pantalla de Inicio</h1>
+        <Link to="/Dashboard">
+          <button>Dashboard</button>
+        </Link>
         <Link to="/creaCita">
           <button>Crear Cita</button>
+        </Link>
+        <Link to="/listarCita">
+          <button>Ver Citas</button>
+        </Link>
+        <Link to="/creaDuenio">
+          <button>Crear Dueño</button>
+        </Link>
+        <Link to="/listarDuenio">
+          <button>Ver Dueños</button>
+        </Link>
+        <Link to="/creaMascota">
+          <button>Crear Mascota</button>
+        </Link>
+        <Link to="/listaMascota">
+          <button>Ver Mascotas</button>
         </Link>
         <Link to="/facturacion">
           <button>Facturación</button>
@@ -40,17 +58,19 @@ function App() {
         <Link to="/facturas">
           <button>Ver Facturas</button>
         </Link>
-        <Link to="/Dashboard">
-          <button>Dashboard</button>
-        </Link>
       </div>
       
       <Routes>
+        <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/creaCita" element={<CrearCita />} />
+        <Route path="/listarCita" element={<ListarCitas />} />
+        <Route path="/creaDuenio" element={<DuenioForm />} />
+        <Route path="/listarDuenio" element={<DuenioList />} />
+        <Route path="/creaMascota" element={<CrearMascota />} />
+        <Route path="/listaMascota" element={<ListarMascotas />} />
         <Route path="/facturacion" element={<Facturacion />} />
         <Route path="/facturas" element={<Facturas />} />
         <Route path="/edit/:id" element={<FacturaEdit />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
